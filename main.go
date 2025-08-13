d4eb05f4f0c95629dd5120741e8954de38439433
macrespo42 <macrespo@student.42.fr>
2025-08-13 22:25:05 +0200

    feat: render article on home page

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 type Article struct {
-	Path         string
-	Title        string
-	Content      string
-	CreationDate time.Time
+	Id           int       `json:"id"`
+	Title        string    `json:"title"`
+	Content      string    `json:"content"`
+	CreationDate time.Time `json:"creationDate"`
+}
+
+func getArticles() ([]Article, error) {
+	files, err := os.ReadDir("./articles")
+	if err != nil {
+		return nil, err
+	}
+
+	var articles []Article
+
+	for _, file := range files {
+		fileData, err := os.ReadFile(filepath.Join("./articles", file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		var article Article
+		err = json.Unmarshal(fileData, &article)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, article)
+	}
+
+	return articles, nil
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +48,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Add("Content-Type", "text/html")
-	err = t.Execute(w, nil)
+	articles, err := getArticles()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, articles)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
