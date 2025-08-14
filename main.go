a953094d3e3524fd9d269e232132d5f76d67d803
macrespo42 <macrespo@student.42.fr>
2025-08-14 18:27:18 +0200

    feat: show article page

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 )
 
@@ -40,6 +42,21 @@ func getArticles() ([]Article, error) {
 	return articles, nil
 }
 
+func getArticleById(id int) (Article, error) {
+	fileName := fmt.Sprintf("%v.json", id)
+	fileData, err := os.ReadFile(filepath.Join("./articles", fileName))
+	if err != nil {
+		return Article{}, err
+	}
+
+	var article Article
+	err = json.Unmarshal(fileData, &article)
+	if err != nil {
+		return Article{}, err
+	}
+	return article, nil
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./templates/base.html", "./templates/home.html")
 	if err != nil {
@@ -60,9 +77,32 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	t, err := template.ParseFiles("./templates/base.html", "./templates/article.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "text/html")
+	idNb, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	article, err := getArticleById(idNb)
+	err = t.Execute(w, article)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/home", homeHandler)
+	router.HandleFunc("/article/{id}", articleHandler)
 	http.ListenAndServe(":8080", router)
 }
